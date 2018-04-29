5a534783cd9e938608b8aebd70c0649eb86a53a9
HeavyMetalPirate <37298940+HeavyMetalPirate@users.noreply.github.com>
2018-04-29 10:52:06 +0200

    code cleanup

--- Fantasy Unlimited Discord/src/main/java/com/fantasyunlimited/discord/xml/items/GenericsBag.java
+++ Fantasy Unlimited Discord/src/main/java/com/fantasyunlimited/discord/xml/items/GenericsBag.java
@@ -1,11 +1,8 @@
 package com.fantasyunlimited.discord.xml.items;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.DirectoryStream;
